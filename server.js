07f72644b6e65cb91004d9ee2395227384f54ec1
CannedShroud <hypersync112@gmail.com>
2020-09-15 23:32:20 +0530

    Connect mongoose to mongoDB

--- server.js
+++ server.js
@@ -5,10 +5,17 @@ import mongoose from "mongoose";
 // config
 const app = express();
 const port = process.env.PORT || 4000;
+const connection_url =
+  "mongodb+srv://admin:mixedfruit@cluster0.uvuef.mongodb.net/tinderDB?retryWrites=true&w=majority";
 
 // midwares
 
 // db conigs
+mongoose.connect(connection_url, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
 
 // routes
 app.get("/", (req, res) => {

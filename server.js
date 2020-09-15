37973521ac801f9992e9bd068182bbdd82f8ea33
CannedShroud <hypersync112@gmail.com>
2020-09-15 23:41:36 +0530

    Create get route to get data from DB

--- server.js
+++ server.js
@@ -22,6 +22,7 @@ mongoose.connect(connection_url, {
 app.get("/", (req, res) => {
   res.status(200).send("Hello World");
 });
+
 app.post("/tinder/cards", (req, res) => {
   const dbCard = req.body;
   Cards.create(dbCard, (err, data) => {
@@ -31,6 +32,15 @@ app.post("/tinder/cards", (req, res) => {
       res.status(201).send(data);
     }
   });
+
+app.get("/tinder/cards", (req, res)=>{
+    Cards.find((err, data) => {
+        if (err) {
+          res.status(500).send(err);
+        } else {
+          res.status(200).send(data);
+        }
+})
 });
 
 // listen

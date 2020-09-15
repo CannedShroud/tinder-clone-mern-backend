dcb426e764b856287f397247383a6acc7628f209
CannedShroud <hypersync112@gmail.com>
2020-09-15 23:25:51 +0530

    Init basic server

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
-// imports 
+// imports
+import express from "express";
+import mongoose from "mongoose";
 
 // config
+const app = express();
+const port = process.env.PORT || 4000;
 
 // midwares
 
+// db conigs
+
 // routes
+app.get("/", (req, res) => {
+  res.status(200).send("Hello World");
+});
 
 // listen
+app.listen(port, () => {
+  console.log("====================================");
+  console.log("Server started on port ->", port);
+  console.log("====================================");
+});
